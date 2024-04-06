df36650797d57da8d19ea0147860b9c28284215c
jeyson <jeysonkmguzman@gmail.com>
2024-04-06 15:18:20 -0500

    add visual studio code

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,44 +1,82 @@
-import Split from 'split-grid'
+import Split from "split-grid";
+import * as monaco from "monaco-editor";
+import htmlVSC from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import cssVSC from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import jsVSC from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 // src="data:text/html;base64,PHA+aG9sYSBtdW5kbzwvcD4="
 
-Split({
-	columnGutters: [{
-    track: 1,
-    element: document.querySelector('.vertical-gutter') as HTMLElement,
-  }],
-  rowGutters: [{
-  	track: 1,
-    element: document.querySelector('.horizontal-gutter') as HTMLElement,
-  }]
-})
+window.MonacoEnvironment = {
+  getWorker(_, label):any {
+    if (label === "html") {
+      return new htmlVSC();
+    }
+    if (label === "css") {
+      return new cssVSC();
+    }
+    if (label === "javascript") {
+      return new jsVSC();
+    }
+  },
+};
 
+Split({
+  columnGutters: [
+    {
+      track: 1,
+      element: document.querySelector(".vertical-gutter") as HTMLElement,
+    },
+  ],
+  rowGutters: [
+    {
+      track: 1,
+      element: document.querySelector(".horizontal-gutter") as HTMLElement,
+    },
+  ],
+});
 
 const getByID = (id: string) => document.getElementById(id);
-const html: HTMLTextAreaElement = getByID("html") as HTMLTextAreaElement;
-const css: HTMLTextAreaElement = getByID("css") as HTMLTextAreaElement;
-const js: HTMLTextAreaElement = getByID("js") as HTMLTextAreaElement;
+
+const html: HTMLElement = getByID("html") as HTMLElement;
+const css: HTMLElement = getByID("css") as HTMLElement;
+const js: HTMLElement = getByID("js") as HTMLElement;
 const iframe: HTMLIFrameElement = getByID("iframe") as HTMLIFrameElement;
 
-const onEvent = (event: Event) => {
-  const element = event.target as HTMLTextAreaElement;
-  console.log(element.value);
+const settingStyle = {
+  automaticLayout: true,
+  theme: "vs-dark",
+  fontSize: 18,
 };
+
+const htmlEditor = monaco.editor.create(html, {
+  value: "",
+  language: "html",
+  ...settingStyle,
+});
+const cssEditor = monaco.editor.create(css, {
+  value: "",
+  language: "css",
+  ...settingStyle,
+});
+const jsEditor = monaco.editor.create(js, {
+  value: "",
+  language: "javascript",
+  ...settingStyle,
+});
 
 const update = () => {
   const getCode = createHtml();
   iframe.setAttribute("srcdoc", getCode);
 };
 
-html?.addEventListener("input", update);
-css?.addEventListener("input", update);
-js?.addEventListener("input", update);
+html?.addEventListener("keydown", update);
+css?.addEventListener("keydown", update);
+js?.addEventListener("keydown", update);
 
 function createHtml() {
-  const htmlScript = html.value;
-  const cssCode = css.value;
-  const jsCode = js.value;
-  console.log(htmlScript);
+  const htmlScript = htmlEditor.getValue();
+  const cssCode = cssEditor.getValue();
+  const jsCode = jsEditor.getValue();
   return `
    <!doctype html>
      <html lang="es">
