ba7b666dbefbae8903d99c56cdff408214e03ec8
jeyson <jeysonkmguzman@gmail.com>
2024-04-05 22:27:53 -0500

    set style and structure editor dev

---
+++ src/script/main.ts
@@ -0,0 +1,42 @@
+// src="data:text/html;base64,PHA+aG9sYSBtdW5kbzwvcD4="
+
+const getByID = (id: string) => document.getElementById(id);
+const html: HTMLTextAreaElement = getByID("html") as HTMLTextAreaElement;
+const css: HTMLTextAreaElement = getByID("css") as HTMLTextAreaElement;
+const js: HTMLTextAreaElement = getByID("js") as HTMLTextAreaElement;
+const iframe: HTMLIFrameElement = getByID("iframe") as HTMLIFrameElement;
+
+const onEvent = (event: Event) => {
+  const element = event.target as HTMLTextAreaElement;
+  console.log(element.value);
+};
+
+const update = () => {
+  const getCode = createHtml();
+  iframe.setAttribute("srcdoc", getCode);
+};
+
+html?.addEventListener("input", update);
+css?.addEventListener("input", update);
+js?.addEventListener("input", update);
+
+function createHtml() {
+  const htmlScript = html.value;
+  const cssCode = css.value;
+  const jsCode = js.value;
+  console.log(htmlScript);
+  return `
+   <!doctype html>
+     <html lang="es">
+     <head>
+       <style>
+         ${cssCode}
+       </style>
+     </head>
+     <body>
+       ${htmlScript}
+       <script>${jsCode}</script>
+     </body>
+   </html>
+   `;
+}
